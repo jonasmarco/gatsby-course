81b3187d50f0f9f8a37ec53f92eac34f49e44e96
Jonas Marco <jonasmarco.ti@gmail.com>
2020-12-28 18:21:58 -0300

    add: estilos sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
 import GlobalStyles from '../../styles/global'
 import * as S from './styled'
@@ -10,9 +10,7 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
