aa476907d74064fa73a6a4a0c8545af1e21b0aae
Jonas Marco <jonasmarco.ti@gmail.com>
2020-12-28 18:04:36 -0300

    add: reset global de css

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,14 +3,29 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Profile from "../Profile"
 
+import GlobalStyles from '../../styles/global'
+import styled from 'styled-components'
+
+const LayoutWrapper = styled.section`
+  display: flex;
+`
+
+const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
+
 const Layout = ({ children }) => {
   return (
-    <Fragment>
+    <LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <main>{children}</main>
-    </Fragment>
+      <LayoutMain>{children}</LayoutMain>
+    </LayoutWrapper>
   )
 }
 
