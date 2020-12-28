7a6c695a831eee1a11d298fee0d49c30fedafda2
Jonas Marco <jonasmarco.ti@gmail.com>
2020-12-28 17:40:35 -0300

    add: imagem em profile

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "profile.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 60) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return <Img fluid={avatarImage.childImageSharp.fluid} />
+}
+
+export default Avatar
