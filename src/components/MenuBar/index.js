4b55f44b0a4c4d7ad4d77f527e318acad2d7cae1
Jonas Marco <jonasmarco.ti@gmail.com>
2021-02-24 22:25:41 -0300

    add: estilos responsivos

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -55,6 +55,7 @@ const MenuBar = () => {
           onClick={() => {
             window.__setPreferredDisplay(isListMode ? "grid" : "list")
           }}
+          className="display"
         >
           {isListMode ? <Grid /> : <List />}
         </S.MenuBarItem>
